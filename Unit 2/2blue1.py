74fb5e90319f20a096de1928780c87f03758d499
SamlKeller <samuel0keller@gmail.com>
2023-11-02 00:08:53 -0400

    Cleaned up code

--- Unit 2/2blue1.py
+++ Unit 2/2blue1.py
@@ -1,23 +1,3 @@
-'''
-1) Write a while loop that makes a list of every positive number that is a multiple of 3 or 5 less than 1000. The list
-should not include the number 0 or the number 1000.
-
-2) Modify the above code to create a function list_mults that accepts two arguments and returns a list of every
-number that is a multiple of 3 or 5 between the two values it receives.
-    o For example, list_mults(100, 111) would return [102, 105, 108, 110]
-
-3) Write a function all_odds that takes a single number and returns True if every single digit of the given
-number is an odd digit. False if any digit is even.
-    o For example, all_odds(153719) would return True
-    o For example, all_odds(1345) would return False, because of the 4
-
-4) Write a function make_fibos that takes a single integer argument and uses a while loop to return a list of all
-the Fibonacci numbers less than that value. (You can assume the value will never be less than 2.)
-    o For example, make_fibos(100) would return [1, 1, 2, 3, 5, 8, 13, 21, 34, 55, 89]
-    o Hint: remember that, no matter how long a list is, list[-1] will always access the last element in it,
-      and list[-2] will always access the second to last element in it, etc.
-'''
-
 def mult35less1000 ():
     list = []
     iterator = 1000
